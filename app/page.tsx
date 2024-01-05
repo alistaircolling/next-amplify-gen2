e138138bf401e9fa3da5d451e18ea170f739c575
Alistair <a@colli.ng>
2024-01-05 18:25:30 +0000

    feat: user sign in and out and form created

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,22 @@
-'use client';
-import TodoList from '../components/Todolist';
+'use client'
+import { signOut } from 'aws-amplify/auth'
+import TodoList from '../components/Todolist'
+import TodoCreateForm from '../ui-components/TodoCreateForm'
 import styles from './page.module.css'
 import { withAuthenticator } from '@aws-amplify/ui-react'
 
 function Home() {
   return (
     <main className={styles.main}>
+      <button style={{ background: 'black', color: 'pink', padding: '20px' }} onClick={() => signOut()}>
+        Sign out
+      </button>
       <h1>Hello 👋</h1>
+      <h2>Create Form</h2>
+      <TodoCreateForm />
+      <hr />
       <TodoList />
+      // signout button
     </main>
   )
 }
